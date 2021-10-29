dda2475edabf4b59a268d8ff6e6d0ad939a731c6
Gabriel <gabriel98123_@hotmail.com>
2021-10-28 23:25:24 -0300

    Finalizado app IMC para disciplina de DSM

--- DesenvolvimentoSistemasMoveis/imc/lib/main.dart
+++ DesenvolvimentoSistemasMoveis/imc/lib/main.dart
@@ -4,7 +4,23 @@ import 'package:imc/views/home_screen.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xffEED1D8),
+      ),
       home: Home(),
     ),
   );
 }
+
+/*
+
+Desenvolva um aplicativo Flutter que irá calcular o Índice de Massa Corpórea (IMC) do usuário. O IMC é reconhecido como padrão internacional para avaliar o grau de sobrepeso e obesidade. É calculado dividindo o peso (em kg) pela altura ao quadrado (em metros).
+
+IMC = peso / (altura*altura)
+
+O aplicativo será composto por dois campos de texto, que terão como objetivo receber as informações necessárias do usuário. O primeiro campo é referente ao valor da altura (em metros) e o segundo referente ao peso (em Kg). O aplicativo deve possuir, também, um botão para disparar a ação de cálculo do IMC. 
+
+Ao final do cálculo, o aplicativo deve apresentar em qual classificação o usuário pertence de acordo com a seguinte tabela. 
+
+ */
--- DesenvolvimentoSistemasMoveis/imc/lib/views/home_screen.dart
+++ DesenvolvimentoSistemasMoveis/imc/lib/views/home_screen.dart
@@ -10,6 +10,49 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController _controllerPeso = TextEditingController();
+  TextEditingController _controllerAltura = TextEditingController();
+  String _resultadoIMC = "";
+
+  void _calcularIMC() {
+    if (double.tryParse(_controllerPeso.text) != null &&
+        double.tryParse(_controllerAltura.text) != null) {
+      double peso = double.parse(_controllerPeso.text);
+      double altura = double.parse(_controllerAltura.text);
+      double imc = peso / (altura * altura);
+
+      if (imc < 18.5) {
+        setState(() {
+          _resultadoIMC = "Abaixo do peso";
+        });
+      } else if (imc >= 18.6 && imc <= 24.9) {
+        setState(() {
+          _resultadoIMC = "Peso Ideal";
+        });
+      } else if (imc >= 25.0 && imc <= 29.9) {
+        setState(() {
+          _resultadoIMC = "Levemente acima do peso";
+        });
+      } else if (imc >= 30.0 && imc <= 34.9) {
+        setState(() {
+          _resultadoIMC = "Obesidade grau 1";
+        });
+      } else if (imc >= 35.0 && imc <= 39.9) {
+        setState(() {
+          _resultadoIMC = "Obesidade grau 2";
+        });
+      } else if (imc > 40) {
+        setState(() {
+          _resultadoIMC = "Obesidade grau 3";
+        });
+      } else {
+        setState(() {
+          _resultadoIMC = "Dados Inválidos";
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,49 +62,63 @@ class _HomeState extends State<Home> {
       ),
       body: Container(
         padding: EdgeInsets.all(32),
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(bottom: 32),
-              child: Image.asset("assets/imgs/imc.png"),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 12),
-              child: Text(
-                "Calcule seu Índice de Massa Corporal",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 20),
+                child: Image.asset("assets/imgs/imc.png"),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 12),
+                child: Text(
+                  "Calcule seu Índice de Massa Corporal",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: "Peso"),
-              style: TextStyle(fontSize: 20),
-              //controller: ,
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: "Altura"),
-              style: TextStyle(fontSize: 20),
-              //controller: ,
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: ElevatedButton(
-                child: Text("Calcular"),
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.black,
-                    padding: EdgeInsets.symmetric(horizontal: 60, vertical: 20),
-                    textStyle: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                    )),
-                onPressed: () {},
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: "Peso"),
+                style: TextStyle(fontSize: 20),
+                controller: _controllerPeso,
               ),
-            ),
-          ],
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: "Altura"),
+                style: TextStyle(fontSize: 20),
+                controller: _controllerAltura,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 15),
+                child: ElevatedButton(
+                  child: Text("Calcular"),
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.purple,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 60, vertical: 10),
+                      textStyle: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  onPressed: _calcularIMC,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 80),
+                child: Text(
+                  _resultadoIMC,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
